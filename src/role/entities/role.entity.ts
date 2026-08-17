a2769042285b43a6e5968c58e98bf7fc3245cca3
mozict <mozict@gmail.com>
2026-08-17 17:35:34 +0700

    fix(db): change unique constraint on roles to composite (name, tenant_id)

--- src/role/entities/role.entity.ts
+++ src/role/entities/role.entity.ts
@@ -8,17 +8,19 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   JoinTable,
+  Index,
 } from 'typeorm';
 import { TenantBaseEntity } from '../../entities/tenant-base.entity';
 import { Permission } from '../../entities/permission.entity';
 import { Menu } from '../../entities/menu.entity';
 
 @Entity('roles')
+@Index(['name', 'tenantId'], { unique: true })
 export class Role extends TenantBaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
+  @Column()
   name: string;
 
   @Column({ nullable: true })
